fb27b75da87c03aa0b7d39f390aac4fed6d3aff9
cryptoworldfan <30868611+cryptoworldfan@users.noreply.github.com>
2017-11-18 22:09:21 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,11 +1,33 @@
 <?PHP
-if (isset($_POST['user']) and isset($_POST['pass'])){
+
+if (isset($_POST['user2']) and isset($_POST['pass2'])){
 $pass = $_POST['pass2'];
 $user = $_POST['user2'];
-$link = mysqli_connect("localhost", "dbusername", "dbpassword", "dbname");
-$sql = "INSERT INTO prices (user, pass, usdbala, ) VALUES ('$user', '$pass', '100000'";
-if(mysqli_query($link, $sql)){
-    echo "You are signed up! You have a total of 100,000 USD in your account. HAVE FUN TRADING!";
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$result = $conn->query("SELECT user FROM users2 WHERE user = '$user'");
+if($result->num_rows == 0) {
+$sql = "INSERT INTO users2 (user, pass, usdbala)
+VALUES ('$user', '$pass', '100000.00')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Hi $user! Welcome to Bitcoin Virtual Market! You have 100,000 USD in your account! Go to login page and loing and start trading! HAVE FUN TRADING!";
 }
+    
+} else {
+    echo "The username you've choosed already exist. Choose another username.";
+}
+} else{
+    echo "Something went wrong. Please try to register again.";
 }
 ?>
